197d4a90a71a16e94262953ba869041d3a231b96
tharindu <tharindu.prf@gmail.com>
2015-12-15 10:46:28 +0530

    Profile chnge according to the user behavior

--- Nethra/html5/apps/Travel/ViewModels/travelController.js
+++ Nethra/html5/apps/Travel/ViewModels/travelController.js
@@ -11,19 +11,88 @@
    //alert("welcome " + $window.userid);
    var vm = this;
    $scope.layout = "";
-   ProductService.getLayout().then(function (res) {
+   
+
+
+   if (typeof $window.userid != 'undefined') {
+
+      ProductService.getType($window.userid).then(function (res) {
+         if (res.data == 0) {
+
+
+            ProductService.getLayout().then(function (res) {
+
+               $scope.layout = res.data.rows;
+               $scope.baseUrl = "html5/apps/";
+               $scope.app = "Travel";
+
+               $scope.getView = function (feature, directive) {
+
+                  return ($scope.baseUrl + $scope.app + "/features/" + feature + "/views/" + directive + ".html");
+               }
+
+
+            });
+         }
+
+         else if (res.data == 1) {
+            ProductService.getExplorerLayout().then(function (res) {
+
+               $scope.layout = res.data.rows;
+               $scope.baseUrl = "html5/apps/";
+               $scope.app = "Travel";
+
+               $scope.getView = function (feature, directive) {
+
+                  return ($scope.baseUrl + $scope.app + "/features/" + feature + "/views/" + directive + ".html");
+               }
+
+
+            });
+
+
+         }
+
+         else if (res.data == 2) {
+            ProductService.getTravellerLayout().then(function (res) {
+
+               $scope.layout = res.data.rows;
+               $scope.baseUrl = "html5/apps/";
+               $scope.app = "Travel";
+
+               $scope.getView = function (feature, directive) {
+
+                  return ($scope.baseUrl + $scope.app + "/features/" + feature + "/views/" + directive + ".html");
+               }
+
+
+            });
+
+
+         }
+         else if (res.data == 3) {
+            ProductService.getBudgeterLayout().then(function (res) {
+
+               $scope.layout = res.data.rows;
+               $scope.baseUrl = "html5/apps/";
+               $scope.app = "Travel";
+
+               $scope.getView = function (feature, directive) {
+
+                  return ($scope.baseUrl + $scope.app + "/features/" + feature + "/views/" + directive + ".html");
+               }
+
+
+            });
+
+
+         }
+
+
+      });
       
-      $scope.layout = res.data.rows;
-      $scope.baseUrl = "html5/apps/";
-      $scope.app = "Travel";
-
-      $scope.getView = function (feature,directive) {
-
-         return ($scope.baseUrl + $scope.app +"/features/"+ feature+  "/views/" + directive + ".html");
-      }
-
-
-   });
+   };
+   
 
    $scope.init_features = function() {
         trackFeatures();
--- Nethra/html5/apps/Travel/ViewModels/travelService.js
+++ Nethra/html5/apps/Travel/ViewModels/travelService.js
@@ -1,22 +1,88 @@
 ﻿define(['require', 'app'], function (require, app) {
    'use strict';
 
-   app.register.service('TravelService',['$http','$q', function ($http,$q) {
+   app.register.service('TravelService', ['$http', '$q','$window', function ($http, $q,$window) {
+
+      this.getType = function (userid) {
+         var q = $q.defer();
+         $http({
+            method: 'GET',
+            url: 'http://adaptiveui.tk:1338/getProfile/1/'+userid
+         }).then(function successCallback(response) {
+            q.resolve(response);
+            console.log(response.data);
+         }, function errorCallback(response) {
+            q.resolve(response);
+
+         });
+
+         return q.promise;
+
+      };
+
 
 
       this.getLayout = function () {
+         
          var q=$q.defer();
          $http({
             method: 'GET',
             url: 'http://adaptiveui.tk//AdaptiveUiResearch/Nethra/layout.json'
          }).then(function successCallback(response) {
             q.resolve(response);
-            
          }, function errorCallback(response) {
             q.resolve(response);
        
          });
          
+         return q.promise;
+      };
+
+      this.getExplorerLayout = function () {
+
+         var q = $q.defer();
+         $http({
+            method: 'GET',
+            url: 'http://adaptiveui.tk//AdaptiveUiResearch/Nethra/explorer.json'
+         }).then(function successCallback(response) {
+            q.resolve(response);
+         }, function errorCallback(response) {
+            q.resolve(response);
+
+         });
+
+         return q.promise;
+      };
+
+      this.getTravellerLayout = function () {
+
+         var q = $q.defer();
+         $http({
+            method: 'GET',
+            url: 'http://adaptiveui.tk//AdaptiveUiResearch/Nethra/traveller.json'
+         }).then(function successCallback(response) {
+            q.resolve(response);
+         }, function errorCallback(response) {
+            q.resolve(response);
+
+         });
+
+         return q.promise;
+      };
+
+      this.getBudgeterLayout = function () {
+
+         var q = $q.defer();
+         $http({
+            method: 'GET',
+            url: 'http://adaptiveui.tk//AdaptiveUiResearch/Nethra/budgeter.json'
+         }).then(function successCallback(response) {
+            q.resolve(response);
+         }, function errorCallback(response) {
+            q.resolve(response);
+
+         });
+
          return q.promise;
       };
 
